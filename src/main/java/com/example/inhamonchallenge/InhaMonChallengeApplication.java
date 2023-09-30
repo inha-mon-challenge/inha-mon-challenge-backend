3613d0003cb76c33011d549b4d579ce20c1da2c0
KimSangHyeok <tkdgur0906@naver.com>
2023-09-30 17:19:57 +0900

    initial commit

---
+++ src/main/java/com/example/inhamonchallenge/InhaMonChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.example.inhamonchallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InhaMonChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InhaMonChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/inhamonchallenge/InhaMonChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inhamonchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InhaMonChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
