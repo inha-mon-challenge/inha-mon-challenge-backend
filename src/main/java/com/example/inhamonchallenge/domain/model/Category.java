e60481db4f40da4e38b43ee89ce6e3258d9b034f
KimSangHyeok <tkdgur0906@naver.com>
2023-10-30 23:52:47 +0900

    feat : Enum 값 임시 저장

--- src/main/java/com/example/inhamonchallenge/domain/model/Category.java
+++ src/main/java/com/example/inhamonchallenge/domain/model/Category.java
@@ -1,4 +1,10 @@
 package com.example.inhamonchallenge.domain.model;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum Category {
+    STUDY("공부");
+
+    private final String category;
 }
--- src/main/java/com/example/inhamonchallenge/domain/model/FeedType.java
+++ src/main/java/com/example/inhamonchallenge/domain/model/FeedType.java
@@ -1,4 +1,11 @@
 package com.example.inhamonchallenge.domain.model;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum FeedType {
+    HABIT("습관"),
+    RECORD("기록");
+
+    private final String feedType;
 }
