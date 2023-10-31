75f2949e1f8501dc182c7ab34cfe41d417c1a131
KimSangHyeok <tkdgur0906@naver.com>
2023-10-31 22:58:56 +0900

    KAN-47 특정 유저의 습관 목록 조회기능 구현

--- src/main/java/com/example/inhamonchallenge/domain/habit/repository/HabitRepository.java
+++ src/main/java/com/example/inhamonchallenge/domain/habit/repository/HabitRepository.java
@@ -3,5 +3,9 @@ package com.example.inhamonchallenge.domain.habit.repository;
 import com.example.inhamonchallenge.domain.habit.domain.Habit;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HabitRepository extends JpaRepository<Habit, Long> {
+
+    List<Habit> findAllByUserId(Long userId);
 }
--- src/main/java/com/example/inhamonchallenge/domain/habit/service/HabitService.java
+++ src/main/java/com/example/inhamonchallenge/domain/habit/service/HabitService.java
@@ -6,23 +6,27 @@ import com.example.inhamonchallenge.domain.habit.dto.SaveHabitRequest;
 import com.example.inhamonchallenge.domain.habit.dto.SaveHabitResponse;
 import com.example.inhamonchallenge.domain.habit.exception.NotFoundHabitException;
 import com.example.inhamonchallenge.domain.habit.repository.HabitRepository;
+import com.example.inhamonchallenge.domain.model.dto.Result;
 import com.example.inhamonchallenge.domain.user.domain.User;
 import com.example.inhamonchallenge.domain.user.exception.NotFoundUserException;
 import com.example.inhamonchallenge.domain.user.repository.UserRepository;
 import com.example.inhamonchallenge.global.security.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class HabitService {
 
     private final HabitRepository habitRepository;
     private final UserRepository userRepository;
 
-    public HabitResponse findHabit(Long habitId) {
+    public HabitResponse getHabit(Long habitId) {
         Habit habit = habitRepository.findById(habitId).orElseThrow(NotFoundHabitException::new);
         return HabitResponse.from(habit, List.of(getHashtagsArray(habit.getHashtags())));
     }
@@ -32,6 +36,17 @@ public class HabitService {
         Habit habit = SaveHabitRequest.toEntity(request, user, "xxx");
         Habit savedHabit = habitRepository.save(habit);
         return SaveHabitResponse.from(savedHabit);
+    }
+
+    public Result<List<HabitResponse>> getAllHabitsByUserId(Long userId){
+        userRepository.findById(userId).orElseThrow(NotFoundUserException::new);
+        List<Habit> habits = habitRepository.findAllByUserId(userId);
+
+        List<HabitResponse> habitResponses = habits.stream()
+                .map(habit -> HabitResponse.from(habit, List.of(getHashtagsArray(habit.getHashtags()))))
+                .collect(Collectors.toList());
+
+        return new Result<>(habitResponses);
     }
 
     private String[] getHashtagsArray(String hashtags) {
