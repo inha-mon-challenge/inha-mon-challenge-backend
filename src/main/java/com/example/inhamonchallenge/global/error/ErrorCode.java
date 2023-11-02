8c82c0a37fe7713172f16dcd24ed2f7a382eeb61
KimSangHyeok <tkdgur0906@naver.com>
2023-11-02 16:39:40 +0900

    fix : 에러 메시지 수정

--- src/main/java/com/example/inhamonchallenge/global/error/ErrorCode.java
+++ src/main/java/com/example/inhamonchallenge/global/error/ErrorCode.java
@@ -11,11 +11,11 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
 
     // 습관
-    NOT_FOUND_MEMBER_EXCEPTION(404, "습관이 존재하지 않습니다.", NOT_FOUND),
+    NOT_FOUND_MEMBER_EXCEPTION(404, "존재하지 않는 습관입니다.", NOT_FOUND),
     //습관기록
-    NOT_FOUND_RECORD_EXCEPTION(404, "습관이 존재하지 않습니다.", NOT_FOUND),
+    NOT_FOUND_RECORD_EXCEPTION(404, "존재하지 않는 습관 기록입니다.", NOT_FOUND),
     //유저
-    NOT_FOUND_USER_EXCEPTION(404, "사용자가 존재하지 않습니다.", NOT_FOUND),
+    NOT_FOUND_USER_EXCEPTION(404, "존재하지 않는 사용자입니다.", NOT_FOUND),
     //공통
     UPDATE_DENIED_EXCEPTION(403, "수정 권한이 없습니다.", FORBIDDEN);
 
