b207f30a38abe498724cc3f2fa5c41aa62be10f4
KimSangHyeok <tkdgur0906@naver.com>
2023-12-20 17:06:05 +0900

    KAN-137 탈퇴 된 회원 조회 안되게 기능 변경

--- src/main/java/com/example/inhamonchallenge/domain/user/repository/UserRepository.java
+++ src/main/java/com/example/inhamonchallenge/domain/user/repository/UserRepository.java
@@ -2,13 +2,19 @@ package com.example.inhamonchallenge.domain.user.repository;
 
 import com.example.inhamonchallenge.domain.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
 
-    Optional<User> findByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
+    boolean existsByEmail(@Param("email") String email);
 
-    Optional<User> findByName(String name);
+    @Query("SELECT u FROM User u WHERE u.email = :email AND u.isDeleted = false")
+    Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.name = :name AND u.isDeleted = false")
+    Optional<User> findByName(@Param("name") String name);
 }
