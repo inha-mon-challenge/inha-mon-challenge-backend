4ce55767e955a67df2186fdfe54117c17333f1ee
KimSangHyeok <tkdgur0906@naver.com>
2023-12-27 17:31:44 +0900

    KAN-46 습관 업로드 시 습관주기 입력 받도록 수정

--- src/main/java/com/example/inhamonchallenge/domain/habit/dto/SaveHabitRequest.java
+++ src/main/java/com/example/inhamonchallenge/domain/habit/dto/SaveHabitRequest.java
@@ -2,6 +2,7 @@ package com.example.inhamonchallenge.domain.habit.dto;
 
 import com.example.inhamonchallenge.domain.common.Category;
 import com.example.inhamonchallenge.domain.common.PrivacySetting;
+import com.example.inhamonchallenge.domain.habit.domain.Frequency;
 import com.example.inhamonchallenge.domain.habit.domain.Habit;
 import com.example.inhamonchallenge.domain.user.domain.User;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,7 @@ public class SaveHabitRequest {
     private String title;
     private String content;
     private String category;
+    private String frequency;
     private List<String> hashTag;
 
     public static Habit toEntity(SaveHabitRequest request, User user) {
@@ -25,6 +27,7 @@ public class SaveHabitRequest {
                 .category(Category.valueOf(request.getCategory()))
                 .content(request.getContent())
                 .title(request.getTitle())
+                .frequency(Frequency.fromString(request.getFrequency()))
                 .hashtags(String.join(",", request.getHashTag()))
                 .totalRecordCnt(0)
                 .photoRecordCnt(0)
