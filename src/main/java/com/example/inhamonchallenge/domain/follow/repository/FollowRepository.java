3c2c6edc048a17ecbd4ed10912634afdf66a7b5c
KimSangHyeok <tkdgur0906@naver.com>
2024-01-01 23:14:59 +0900

    KAN-149 유저의 팔로잉 개수를 조회하는 기능 구현

--- src/main/java/com/example/inhamonchallenge/domain/follow/repository/FollowRepository.java
+++ src/main/java/com/example/inhamonchallenge/domain/follow/repository/FollowRepository.java
@@ -24,5 +24,8 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     @Query("SELECT f FROM Follow f WHERE f.following.id = :userId AND f.status = 'PENDING' AND f.follower.isDeleted = false")
     List<Follow> findAllByFollowingIdAndStatus(@Param("userId") Long userId);
 
+    @Query("SELECT COUNT(f) FROM Follow f WHERE f.follower.id = :userId AND f.status = 'ACCEPTED'")
+    Long countByFollowerIdAndStatusAccepted(@Param("userId") Long userId);
+
     void deleteByFollowerIdOrFollowingId(Long id, Long id1);
 }
